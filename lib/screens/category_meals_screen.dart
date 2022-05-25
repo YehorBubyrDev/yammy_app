6f5668a5e355803292fc67001a2a809fabb91f4c
YehorBubyrDev <yehor.bubyr@uplight.com>
2022-05-25 14:03:03 +0300

    manage list of meals to display it -without styles

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import '../data/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  static String routeName = '/category-meals';
+
+  const CategoryMealsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -8,13 +12,21 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('$categoryTitle'),
-      ),
-      body: const Center(
-        child: Text('Display recipes for costumer'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('$categoryTitle'),
+        ),
+        body: ListView.builder(
+          itemBuilder: ((context, index) {
+            return Text(
+              categoryMeals[index].title,
+              style: const TextStyle(color: Colors.black),
+            );
+          }),
+          itemCount: categoryMeals.length,
+        ));
   }
 }
